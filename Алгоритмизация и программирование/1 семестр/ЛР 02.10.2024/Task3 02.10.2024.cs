7c45472dfe15dc9e5f9a3a92c13b9c35ee674ccd
KaPie2 <117990421+KaPie2@users.noreply.github.com>
2024-10-02 16:21:04 +0600

    Create Task3 02.10.2024.cs

---
+++ Алгоритмизация и программирование/1 семестр/ЛР 02.10.2024/Task3 02.10.2024.cs
@@ -0,0 +1,32 @@
+//На вход подается последовательность из n элементов (длина не менее 3х)
+//Определить второй максимальный элемент
+
+using System;
+
+class Task3
+{
+	static void Main()
+	{
+		int curr_num, first_max, second_max;
+		Console.Write("Введите количество элементов: ");
+		int n = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine($"Введите {n} целых чисел");
+        curr_num = Convert.ToInt32(Console.ReadLine());
+		first_max = curr_num;
+		second_max = curr_num;
+        for (int i = 1; i < n; i++)
+		{
+            curr_num = Convert.ToInt32(Console.ReadLine());
+			if (curr_num > first_max)
+			{
+				second_max = first_max;
+				first_max = curr_num;
+			} else if (curr_num > second_max && curr_num < first_max)
+			{
+				second_max = curr_num;
+			}
+        }
+        //Console.WriteLine($"1-ый максимальный элемент: {first_max}");
+        Console.WriteLine($"Второй максимальный элемент: {second_max}");
+    }
+}
