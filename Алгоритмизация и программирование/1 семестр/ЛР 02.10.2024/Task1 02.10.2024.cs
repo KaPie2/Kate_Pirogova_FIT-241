44c64ceb8f008c1a2f4cb3bc8e9d7c469b3baeb9
KaPie2 <117990421+KaPie2@users.noreply.github.com>
2024-10-06 12:16:16 +0600

    Update Task1 02.10.2024.cs

--- Алгоритмизация и программирование/1 семестр/ЛР 02.10.2024/Task1 02.10.2024.cs
+++ Алгоритмизация и программирование/1 семестр/ЛР 02.10.2024/Task1 02.10.2024.cs
@@ -5,27 +5,28 @@ using System;
 
 class Task1
 {
-	static void Main()
-	{
-		int last_num, curr_num, next_num, cnt_local_max = 0;
-		Console.Write("Введите количество элементов: ");
-		int n = Convert.ToInt32(Console.ReadLine());
-		Console.WriteLine($"Введите {n} целых чисел");
-		last_num = Convert.ToInt32(Console.ReadLine());
-		curr_num = Convert.ToInt32(Console.ReadLine());
+    static void Main()
+    {
+        int last_num, curr_num, next_num, cnt_local_max = 0;
+        Console.Write("Введите количество элементов: ");
+        int n = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine($"Введите {n} целых чисел");
+        last_num = Convert.ToInt32(Console.ReadLine());
+        curr_num = Convert.ToInt32(Console.ReadLine());
         for (int i = 3; i <= n; i++)
-		{
-			next_num = Convert.ToInt32(Console.ReadLine());
-			if (n == 3)
-			{
-				cnt_local_max = 1;
-			} else if (curr_num > next_num && curr_num > last_num)
-			{
-				cnt_local_max++;
-				last_num = curr_num;
-				curr_num = next_num;
-			}
+        {
+            next_num = Convert.ToInt32(Console.ReadLine());
+            if (curr_num > next_num && curr_num > last_num)
+            {
+                cnt_local_max++;
+                last_num = curr_num;
+                curr_num = next_num;
+            } else
+            {
+                last_num = curr_num;
+                curr_num = next_num;
+            }
         }
-		Console.WriteLine($"Кол-во локальных максимумов: {cnt_local_max}");
-	}
+        Console.WriteLine($"Кол-во локальных максимумов: {cnt_local_max}");
+    }
 }
