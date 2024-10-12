4fa72cdb5a200654d1c7c2e92349399603f51304
KaPie2 <117990421+KaPie2@users.noreply.github.com>
2024-10-12 11:21:50 +0600

    Update Task3 02.10.2024.cs

--- Алгоритмизация и программирование/1 семестр/ЛР 02.10.2024/Task3 02.10.2024.cs
+++ Алгоритмизация и программирование/1 семестр/ЛР 02.10.2024/Task3 02.10.2024.cs
@@ -5,28 +5,36 @@ using System;
 
 class Task3
 {
-	static void Main()
-	{
-		int curr_num, first_max, second_max;
-		Console.Write("Введите количество элементов: ");
-		int n = Convert.ToInt32(Console.ReadLine());
+    static void Main()
+    {
+        int curr_num, first_max, second_max;
+        Console.Write("Введите количество элементов: ");
+        int n = Convert.ToInt32(Console.ReadLine());
         Console.WriteLine($"Введите {n} целых чисел");
         curr_num = Convert.ToInt32(Console.ReadLine());
-		first_max = curr_num;
-		second_max = curr_num;
-        for (int i = 1; i < n; i++)
-		{
-            curr_num = Convert.ToInt32(Console.ReadLine());
-			if (curr_num > first_max)
-			{
-				second_max = first_max;
-				first_max = curr_num;
-			} else if (curr_num > second_max && curr_num < first_max)
-			{
-				second_max = curr_num;
-			}
+        first_max = curr_num;
+        curr_num = Convert.ToInt32(Console.ReadLine());
+        if (curr_num > first_max)
+        {
+            second_max = first_max;
+            first_max = curr_num;
+        } else
+        {
+            second_max = curr_num;
         }
-        //Console.WriteLine($"1-ый максимальный элемент: {first_max}");
+        for (int i = 2; i < n; i++)
+        {
+            curr_num = Convert.ToInt32(Console.ReadLine());
+            if (curr_num > first_max)
+            {
+                second_max = first_max;
+                first_max = curr_num;
+            } else if (curr_num > second_max && curr_num < first_max)
+            {
+                second_max = curr_num;
+            }
+        }
+        Console.WriteLine($"1-ый максимальный элемент: {first_max}");
         Console.WriteLine($"Второй максимальный элемент: {second_max}");
     }
 }
