2c54909fc9be103bab6b08376c1765dff2121dc3
KaPie2 <117990421+KaPie2@users.noreply.github.com>
2024-10-19 19:01:48 +0600

    Update Task1 18.10.2024.cs

--- Алгоритмизация и программирование/1 семестр/ЛР 18.10.2024/Task1 18.10.2024.cs
+++ Алгоритмизация и программирование/1 семестр/ЛР 18.10.2024/Task1 18.10.2024.cs
@@ -2,7 +2,7 @@
 //Признак окончания последовательности – введенное отрицательное или нулевое число. 
 //Необходимо каждый элемент последовательности перевернуть, удалив все нечетные цифры. 
 //Если четных цифр в числе не было, выдать сообщение «Нет четных цифр». 
-//Вложенные циклы while, не знаем строки и for
+//Вложенные циклы while, без строк и for
 
 using System;
 
@@ -18,26 +18,28 @@ class Task1
         {
             new_num = 0;
             flag = false;
-            cnt_digit_minus_one = (int)Math.Log10(num);
-            cnt_null = (int)Math.Pow(10, cnt_digit_minus_one);
+            cnt_digit_minus_one = (int)Math.Log10(num); //количество разрядов - 1
+            cnt_null = (int)Math.Pow(10, cnt_digit_minus_one); //10 в определенной степени для разложения числа по разрядам, начиная с наибольшего
             while (cnt_null > 0)
             {
-                end_num = num % 10;
+                end_num = num % 10; //последняя цифра числа
                 if (end_num % 2 == 0)
                 {
-                    flag = true;
+                    flag = true; //есть четная цифра
                     new_num += (end_num * cnt_null);
-                } else
-                {
-                    new_num /= 10;
                 }
-                cnt_null /= 10;
-                num /= 10;
+                else
+                {
+                    new_num /= 10; //уменьшение кол-ва разрядов числа
+                }
+                cnt_null /= 10; //уменьшение разряда
+                num /= 10; //отсечение последней цифры числа
             }
             if (!flag)
             {
                 Console.WriteLine("Нет четных цифр!");
-            } else Console.WriteLine(new_num);
+            }
+            else Console.WriteLine(new_num);
             num = Convert.ToInt32(Console.ReadLine());
         }
     }
