fb653321c85b52fea45e29583fd5afa754a2b645
KaPie2 <117990421+KaPie2@users.noreply.github.com>
2024-10-30 22:40:42 +0600

    Update Task1 30.10.2024.cs

--- Алгоритмизация и программирование/1 семестр/ЛР 30.10.2024/Task1 30.10.2024.cs
+++ Алгоритмизация и программирование/1 семестр/ЛР 30.10.2024/Task1 30.10.2024.cs
@@ -3,23 +3,25 @@
 using System;
 
 class Task1
-{
-    static void Main()
     {
-        int[] num = new int[6];
-        int cnt = 0;
-        Console.WriteLine("Введите 6 целых чисел");
-        for (int i = 0; i < 6; i++)
+        static void Main()
         {
-            num[i] = Convert.ToInt32(Console.ReadLine());
-        }
-        foreach(int el in num)
-        {
-            if (Math.Abs(el) % 10 == 3)
+            Console.Write("Введите количество элементов: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            int[] num = new int[n];
+            int cnt = 0;
+            Console.WriteLine($"Введите {n} целых чисел");
+            for (int i = 0; i < n; i++)
             {
-                cnt++;
+                num[i] = Convert.ToInt32(Console.ReadLine());
             }
+            foreach (int el in num)
+            {
+                if (Math.Abs(el) % 10 == 3)
+                {
+                    cnt++;
+                }
+            }
+            Console.WriteLine($"Количество чисел, заканчивающихся на 3: {cnt}");
         }
-        Console.WriteLine($"Количество чисел, заканчивающихся на 3: {cnt}");
     }
-}
