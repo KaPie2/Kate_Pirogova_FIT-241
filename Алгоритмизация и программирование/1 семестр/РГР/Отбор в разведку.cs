4cbf484cebe378953294916e4e5f01f54a3ca0bd
KaPie2 <117990421+KaPie2@users.noreply.github.com>
2024-11-23 21:11:27 +0600

    Update Отбор в разведку.cs

--- Алгоритмизация и программирование/1 семестр/РГР/Отбор в разведку.cs
+++ Алгоритмизация и программирование/1 семестр/РГР/Отбор в разведку.cs
@@ -4,29 +4,20 @@ using System;
 
 class Test
 {
+    static int Division(int n)
+    {
+        if (n == 3) return 1;
+        if (n < 3) return 0;
+
+        int even = n / 2;
+        int odd = n - even;
+        
+        return Division(even) + Division(odd);
+    }
     static void Main()
     {
         Console.Write("Введите количество солдат: ");
         int n = Convert.ToInt32(Console.ReadLine());
-        int cnt = 0, curr_cnt = 0, even = 0, odd = 0;
-
-        Queue<int> groups = new Queue<int>();
-        groups.Enqueue(n);
-
-        while (groups.Count > 0)
-        {
-            curr_cnt = groups.Dequeue(); //присваивается и удаляется из очереди
-            if (curr_cnt == 3) cnt++; //если солдат трое, то прибавляем к счетчику
-            else if (curr_cnt > 3) //если солдат в группе >3, продолжаем делить на чет и нечет
-            {
-                even = curr_cnt / 2;
-                odd = curr_cnt - even;
-
-                //добавляем в очередь
-                groups.Enqueue(even);
-                groups.Enqueue(odd);
-            }
-        }
-        Console.WriteLine($"Количество групп по 3 солдата: {cnt}");
+        Console.WriteLine($"Количество групп по 3 солдата: {Division(n)}");
     }
 }
