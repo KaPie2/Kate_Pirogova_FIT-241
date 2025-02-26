3a41a655e0a912fefb4ce1984310e7b90cf6e7c8
KaPie2 <117990421+KaPie2@users.noreply.github.com>
2025-02-26 10:21:03 +0600

    Create Task1 26.02.2025.cs

---
+++ Алгоритмизация и программирование/2 семестр/ЛР 26.02.2025/Task1 26.02.2025.cs
@@ -0,0 +1,42 @@
+//1.На вход подается последовательность, состоящая из чисел, знаков математических и трех видов скобок (круглые квадратные и фигурные).
+//Определить, правильно ли расставлены скобки в заданной последовательности с использованием элемента стек.
+//Если скобка открывающая, то мы помещаем в стек, если закрывающая, то берем верхушку стека и проверяем.
+
+
+using System;
+using System.Collections;
+using System.Timers;
+
+namespace Task1
+{
+    class Programm
+    {
+        static void Main()
+        {
+            Console.Write("Введите последовательность: ");
+            string sequence = Console.ReadLine();
+            string opening_brackets = "([{";
+            string closing_brackets = ")]}";
+            Stack stackOfBrackets = new Stack();
+            bool flag = true;
+            foreach (char el in sequence)
+            {
+                if (opening_brackets.Contains(el)) stackOfBrackets.Push(el);
+                if (closing_brackets.Contains(el) && stackOfBrackets.Count != 0)
+                {
+                    if ((stackOfBrackets.Peek().Equals('(') && el == ')') || (stackOfBrackets.Peek().Equals('[') && el == ']') || (stackOfBrackets.Peek().Equals('{') && el == '}'))
+                    {
+                        stackOfBrackets.Pop();
+                    }
+                    else
+                    {
+                        Console.WriteLine("Скобки расставлены неправильно!");
+                        flag = false;
+                        break;
+                    }
+                }
+            }
+            if (flag) Console.WriteLine("Скобки расставлены правильно!");
+        }
+    }
+}
