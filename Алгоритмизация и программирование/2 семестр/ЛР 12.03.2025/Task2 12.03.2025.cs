ad4d387779e90f2127e192d595004e2ddf7de9e2
KaPie2 <117990421+KaPie2@users.noreply.github.com>
2025-03-16 12:21:07 +0600

    Update Task2 12.03.2025.cs

--- Алгоритмизация и программирование/2 семестр/ЛР 12.03.2025/Task2 12.03.2025.cs
+++ Алгоритмизация и программирование/2 семестр/ЛР 12.03.2025/Task2 12.03.2025.cs
@@ -19,16 +19,26 @@ namespace Task2
             this.Num2 = Num2;
         }
 
-        public delegate int MathOperation(int x, int y);
-
         public int Summa(int x, int y) { return x + y; }
         public int Difference(int x, int y) { return x - y; }
         public int Multiplication(int x, int y) { return x * y; }
         public int Division(int x, int y) { return x / y; }
+
+        public int FirstSequence(int x, int y)
+        {
+            return Multiplication(Difference(Summa(x, y), y), y);
+        }
+
+        public int SecondSequence(int x, int y)
+        {
+            return Division(Difference(Multiplication(x, y), x), x);
+        }
     }
 
     class Programm
     {
+        public delegate int MathOperation(int x, int y);
+
         static void Main()
         {
             Console.Write("Введите первое число: ");
@@ -36,26 +46,15 @@ namespace Task2
             Console.Write("Введите второе число: ");
             int num2 = Convert.ToInt32(Console.ReadLine());
 
-            var calc1 = new Calculate(num1, num2);
-            var calc2 = new Calculate(num1, num2);
+            Calculate calc = new Calculate(num1, num2);
 
-            Calculate.MathOperation move1 = calc1.Summa;
-            int result1 = move1(calc1.Num1, calc1.Num2); // Сложение: num1 + num2
-            move1 = calc1.Difference;
-            result1 = move1(result1, calc1.Num2); // Вычитание: (num1 + num2) - num2
-            move1 = calc1.Multiplication;
-            result1 = move1(result1, calc1.Num2); // Умножение: ((num1 + num2) - num2) * num2
-            Console.WriteLine($"Результат первой цепочки: {result1}");
+            MathOperation firstSequence = calc.FirstSequence;
+            MathOperation secondSequence = calc.SecondSequence;
 
-            Calculate.MathOperation move2 = calc2.Multiplication;
-            int result2 = move2(calc2.Num1, calc2.Num2); // Умножение: num1 * num2
-            move2 = calc2.Difference;
-            result2 = move2(result2, calc2.Num1); // Вычитание: (num1 * num2) - num1
-            move2 = calc2.Division;
+            Console.WriteLine($"Результат первой цепочки: {firstSequence(num1, num2)}");
             if (num1 != 0)
             {
-                result2 = move2(result2, calc2.Num1); // Деление: ((num1 * num2) - num1) / num1
-                Console.WriteLine($"Результат второй цепочки: {result2}");
+                Console.WriteLine($"Результат второй цепочки: {secondSequence(num1, num2)}");
             }
             else Console.WriteLine("Ошибка: деление на ноль невозможно!");
         }
