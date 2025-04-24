f32001233886a9622c9dbb13ee65daf627c64658
KaPie2 <117990421+KaPie2@users.noreply.github.com>
2025-04-24 10:23:11 +0600

    Update Минное поле.cs

--- Алгоритмизация и программирование/2 семестр/РГР/Минное поле.cs
+++ Алгоритмизация и программирование/2 семестр/РГР/Минное поле.cs
@@ -3,86 +3,89 @@
 using System;
 using System.Collections.Generic;
 
-class Program
+namespace Minefield
 {
-    static void Main()
+    class Program
     {
-        Console.Write("Введите размер минного поля через пробел: ");
-        string[] data = Console.ReadLine().Trim().Split();
-        int n = Convert.ToInt32(data[0]);
-        int m = Convert.ToInt32(data[1]);
-
-        int[,] mines = new int[n, m];
-        for (int i = 0; i < n; i++)
+        static void Main()
         {
-            Console.Write($"Введите {m} мин через пробел для {i + 1} ряда: ");
-            string[] row = Console.ReadLine().Trim().Split();
+            Console.Write("Введите размер минного поля через пробел: ");
+            string[] data = Console.ReadLine().Trim().Split();
+            int n = Convert.ToInt32(data[0]);
+            int m = Convert.ToInt32(data[1]);
+
+            int[,] mines = new int[n, m];
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write($"Введите {m} мин через пробел для {i + 1} ряда: ");
+                string[] row = Console.ReadLine().Trim().Split();
+                for (int j = 0; j < m; j++)
+                {
+                    mines[i, j] = Convert.ToInt32(row[j]);
+                }
+            }
+
+            int[,] min_time = new int[n, m];
+            int[,] way = new int[n, m];
+
             for (int j = 0; j < m; j++)
             {
-                mines[i, j] = Convert.ToInt32(row[j]);
+                min_time[0, j] = mines[0, j];
+                way[0, j] = -1; // нет предка для первой строки
             }
-        }
 
-        int[,] min_time = new int[n, m];
-        int[,] way = new int[n, m];
-
-        for (int j = 0; j < m; j++)
-        {
-            min_time[0, j] = mines[0, j];
-            way[0, j] = -1; // нет предка для первой строки
-        }
-
-        for (int row = 1; row < n; row++)
-        {
-            for (int col = 0; col < m; col++)
+            for (int row = 1; row < n; row++)
             {
-                int min_curr = min_time[row - 1, col];
-                int prev_col = col;
-
-                if (col - 1 >= 0 && min_time[row - 1, col - 1] < min_curr)
+                for (int col = 0; col < m; col++)
                 {
-                    min_curr = min_time[row - 1, col - 1];
-                    prev_col = col - 1;
-                }
+                    int min_curr = min_time[row - 1, col];
+                    int prev_col = col;
 
-                if (col + 1 < m && min_time[row - 1, col + 1] < min_curr)
-                {
-                    min_curr = min_time[row - 1, col + 1];
-                    prev_col = col + 1;
-                }
+                    if (col - 1 >= 0 && min_time[row - 1, col - 1] < min_curr)
+                    {
+                        min_curr = min_time[row - 1, col - 1];
+                        prev_col = col - 1;
+                    }
 
-                min_time[row, col] = mines[row, col] + min_curr;
-                way[row, col] = prev_col;
+                    if (col + 1 < m && min_time[row - 1, col + 1] < min_curr)
+                    {
+                        min_curr = min_time[row - 1, col + 1];
+                        prev_col = col + 1;
+                    }
+
+                    min_time[row, col] = mines[row, col] + min_curr;
+                    way[row, col] = prev_col;
+                }
             }
-        }
 
-        int min_total = min_time[n - 1, 0];
-        int min_col = 0;
+            int min_total = min_time[n - 1, 0];
+            int min_col = 0;
 
-        for (int j = 1; j < m; j++)
-        {
-            if (min_time[n - 1, j] < min_total)
+            for (int j = 1; j < m; j++)
             {
-                min_total = min_time[n - 1, j];
-                min_col = j;
+                if (min_time[n - 1, j] < min_total)
+                {
+                    min_total = min_time[n - 1, j];
+                    min_col = j;
+                }
             }
-        }
 
-        // Восстанавливаем путь
-        List<int> result = new List<int>();
-        int curr_col = min_col;
+            // Восстанавливаем путь
+            List<int> result = new List<int>();
+            int curr_col = min_col;
 
-        for (int i = n - 1; i >= 0; i--)
-        {
-            result.Add(curr_col + 1);
-            curr_col = way[i, curr_col];
-        }
-        result.Reverse();
+            for (int i = n - 1; i >= 0; i--)
+            {
+                result.Add(curr_col + 1);
+                curr_col = way[i, curr_col];
+            }
+            result.Reverse();
 
-        Console.WriteLine("Оптимальный путь:");
-        foreach (int col in result)
-        {
-            Console.WriteLine(col);
+            Console.WriteLine("Оптимальный путь:");
+            foreach (int col in result)
+            {
+                Console.WriteLine(col);
+            }
         }
     }
 }
